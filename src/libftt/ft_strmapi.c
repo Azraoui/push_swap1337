6ffdbc923a85a6ca9ef36543124be83cfd6764e2
Azraoui <36798587+Azraoui@users.noreply.github.com>
2021-05-23 20:13:42 +0100

    Add files via upload

---
+++ src/libftt/ft_strmapi.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-azra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/21 18:58:23 by ael-azra          #+#    #+#             */
+/*   Updated: 2019/10/29 21:36:33 by ael-azra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*str;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = 0;
+	if (!s)
+		return (NULL);
+	while (s[len])
+		len++;
+	str = (char*)malloc(sizeof(char) * len + 1);
+	if (str == NULL || f == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
---
+++ src/libftt/ft_substr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-azra <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/18 21:16:28 by ael-azra          #+#    #+#             */
+/*   Updated: 2019/10/27 21:14:56 by ael-azra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char	const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	size_t	i;
+
+	if (!s)
+		return (NULL);
+	len = (start > ft_strlen(s)) ? 0 : len;
+	str = (char*)malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start])
+	{
+		str[i] = s[start];
+		start++;
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
